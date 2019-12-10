2d77a090eb62fcdf8d0dbda41e04d8a6bc85095c
Arthur E. Jones <PartyLich@gmail.com>
2019-12-09 22:53:43 -0500

    refactor: rewrite random-color

--- src/random-color.js
+++ src/random-color.js
@@ -2,21 +2,12 @@
 /* eslint-disable */
 import color from 'color';
 
-
-var ratio = 0.618033988749895;
-var hue   = 0.65;
+const ratio = 0.618033988749895;
+const hue = (0.65 + ratio) % 1;
 
 export default function(saturation, value) {
-  hue += ratio;
-  hue %= 1;
-
-  if (typeof saturation !== 'number') {
-    saturation = 0.5;
-  }
-
-  if (typeof value !== 'number') {
-    value = 0.95;
-  }
+  if (typeof saturation !== 'number') saturation = 0.5;
+  if (typeof value !== 'number') value = 0.95;
 
   return color({
     h: hue * 360,
