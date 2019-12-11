000d107dcba1c7e06eae1885705eee072da3593a
Arthur E. Jones <PartyLich@gmail.com>
2019-12-10 21:52:46 -0500

    refactor: extract LeftMenu component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as CutControls } from './CutControls';
 export { default as DragDropField } from './DragDropField';
+export { default as LeftMenu } from './LeftMenu';
 export { default as Player } from './Player';
 export { default as TimelineSeg } from './TimelineSeg';
