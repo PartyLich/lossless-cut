0a664f7fb4682acb29e9550d4490b06cc1c9d53b
Arthur <partylich@gmail.com>
2019-12-17 13:50:20 -0500

    refactor: extract TimelineSeg styles

--- src/components/TimelineSeg.jsx
+++ src/components/TimelineSeg.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// TODO: import stylesheet
+
+const startMarkerClass = 'cut-time-marker--start';
+const endMarkerClass = 'cut-time-marker--end';
+
 const TimelineSeg = ({
   isCutRangeValid, duration: durationRaw, cutStartTime, cutEndTime, apparentCutStart,
   apparentCutEnd, isActive, segNum, onSegClick, color,
@@ -8,32 +13,22 @@ const TimelineSeg = ({
   const markerWidth = 4;
   const duration = durationRaw || 1;
   const cutSectionWidth = `calc(${((apparentCutEnd - apparentCutStart) / duration) * 100}% - ${markerWidth * 2}px)`;
-
   const startTimePos = `${(apparentCutStart / duration) * 100}%`;
   const endTimePos = `${(apparentCutEnd / duration) * 100}%`;
   const markerBorder = isActive ? `2px solid ${color.string()}` : undefined;
-  const markerBorderRadius = 5;
 
   const startMarkerStyle = {
     background: color.alpha(0.5).string(),
-    width: markerWidth,
     left: startTimePos,
     borderLeft: markerBorder,
-    borderTopLeftRadius: markerBorderRadius,
-    borderBottomLeftRadius: markerBorderRadius,
   };
   const endMarkerStyle = {
     background: color.alpha(0.5).string(),
-    width: markerWidth,
-    marginLeft: -markerWidth,
     left: endTimePos,
     borderRight: markerBorder,
-    borderTopRightRadius: markerBorderRadius,
-    borderBottomRightRadius: markerBorderRadius,
   };
   const cutSectionStyle = {
     background: color.alpha(0.5).string(),
-    marginLeft: markerWidth,
     left: startTimePos,
     width: cutSectionWidth,
   };
@@ -43,7 +38,13 @@ const TimelineSeg = ({
   return (
     <React.Fragment>
       {cutStartTime !== undefined && (
-        <div style={startMarkerStyle} className="cut-time-marker" role="button" tabIndex="0" onClick={onThisSegClick} />
+        <div 
+          style={startMarkerStyle} 
+          className={`cut-time-marker ${startMarkerClass}`}
+          role="button" 
+          tabIndex="0" 
+          onClick={onThisSegClick} 
+        />
       )}
       {isCutRangeValid && (cutStartTime !== undefined || cutEndTime !== undefined) && (
         <div
@@ -55,7 +56,13 @@ const TimelineSeg = ({
         />
       )}
       {cutEndTime !== undefined && (
-        <div style={endMarkerStyle} className="cut-time-marker" role="button" tabIndex="0" onClick={onThisSegClick} />
+        <div 
+          style={endMarkerStyle}
+          className={`cut-time-marker ${endMarkerClass}`}
+          role="button"
+          tabIndex="0"
+          onClick={onThisSegClick}
+        />
       )}
     </React.Fragment>
   );
