8f73774c456cb0193283b7a071a511552ed35581
Arthur E. Jones <PartyLich@gmail.com>
2020-02-15 21:02:42 -0500

    refactor: replace useStore hook with redux configureStore

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,9 +1,17 @@
 import React, { useReducer } from 'react';
+import { createStore } from 'redux';
 
 import rootReducer from './reducers';
 
 
-export default function configureStore(initialState) {
+export default function useStore(initialState) {
   const [state, dispatch] = useReducer(rootReducer, initialState, rootReducer);
   return { state, dispatch };
+}
+
+let store = null;
+
+export function configureStore(initialState) {
+  if (!store) store = createStore(rootReducer, initialState);
+  return store;
 }
