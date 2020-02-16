658a0e856b6f026200143dd152f4df3dab736121
Arthur E. Jones <PartyLich@gmail.com>
2020-02-15 19:17:29 -0500

    refactor: PlayButton flow types

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,13 +1,18 @@
+// @flow
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import { IconButton } from '.';
 
+type Props = {
+  clickHandler: function,
+  playing: boolean,
+}
 
 const PlayButton = ({
   clickHandler,
   playing,
-}) => (
+}: Props) => (
   <IconButton
     icon={playing ? 'fa-pause' : 'fa-play'}
     clickHandler={clickHandler}
