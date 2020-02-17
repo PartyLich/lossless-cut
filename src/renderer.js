3b6f39333a6a8bd24b083aa18a40502693f3d61f
Arthur E. Jones <PartyLich@gmail.com>
2020-02-17 00:21:55 -0500

    refactor: use persistence module for app settings

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,4 @@
+// @flow
 import { ipcRenderer, remote } from 'electron';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,16 +6,20 @@ import { Provider } from 'react-redux';
 
 import { App } from './components';
 import { configureStore } from './configureStore';
+import loadState from './persistence';
 
 
-const store = configureStore();
+const store = configureStore(loadState());
+const container = document.getElementById('app');
 
 function initialRender(event) {
+  if (!container) throw Error('App container is missing from HTML');
+
   ReactDOM.render(
     <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('app')
+        <App />
+      </Provider>,
+      container
   );
 
   ipcRenderer.send('renderer-ready');
